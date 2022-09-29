e2330958fd7f3a4d6ba3e6772797be2da5578fb4
William Santos <thespation@gmail.com>
2022-09-29 13:14:44 -0300

    Update instalar.sh

--- scripts/instalar.sh
+++ scripts/instalar.sh
@@ -8,30 +8,36 @@ VERD="\033[0;32m"	#Deixa a saída na cor verde
 NORM="\033[0m"		#Volta para a cor padrão
 # Alias de identificação de distro
 INXI=`inxi -S` 				#Necessário para identifiar a DE (xfce ou gnome)
-RELEASE=`lsb_release -r`	#Identifica a versão da distro
+RELEASE=`lsb_release -r`		#Identifica a versão da distro
 VERU="22.04" 				#Utual versão do Ubuntu
 ID=`lsb_release -i`			#Identifica qual distro é
-PAST="/tmp/dpux_bspwm/scripts/"
+GIT='https://raw.githubusercontent.com/thespation/dpux_bspwm/main/scripts/' #Pasta dos scripts
+CURL='curl -s'				#Comando para execução do scritp, sem baixar
 
 # Verificação da distro base
 VERI () {
 		if [[ ${INXI} = *Xfce* && ${INXI} = ${VERU} && ${INXI} = *Ubuntu* ]]; then #Testa se é Xubuntu
-			inxi -S ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
-			curl -s https://raw.githubusercontent.com/thespation/dpux_bspwm/main/scripts/base.sh | bash
+			${INXI} ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			{CURL} ${GIT}base.sh | bash
 			#./scripts/xubuntu.sh
 		elif [[ $ID = "Distributor ID:	Pop" && $RELEASE = "Release:	22.04" ]]; then #Testa se é o PopOs
-			inxi -S ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			${INXI} ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			{CURL} ${GIT}base.sh | bash
 		elif [[ ${INXI} = *Xfce* && "Distributor ID:	Debian" && (${RELEASE} = "Release:	testing" || ${RELEASE} = "Release:	11") ]]; then #Testa se é Debian xfce
-			inxi -S ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			${INXI} ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			{CURL} ${GIT}base.sh | bash
 			${PAST}base.sh
 		elif [[ ${INXI} = *GNOME* && ${INXI} = ${VERU} && ${INXI} = *Ubuntu* ]]; then #Testa se é Ubuntu
-			inxi -S ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			${INXI} ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			{CURL} ${GIT}base.sh | bash
 			#./scripts/ubuntu.sh
 		elif [[ ${INXI} = *GNOME* && "Distributor ID:	Debian" && (${RELEASE} = "Release:	testing" || ${RELEASE} = "Release:	11") ]]; then #Testa se é Debian GNOME
-			inxi -S ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			${INXI} ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			{CURL} ${GIT}base.sh | bash
 			#./scripts/debiangnome.sh
 		elif [[ ${INXI} = *GNOME* && ${INXI} = *Fedora* ]]; then
-			inxi -S ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			${INXI} ; echo -e "\n${VERD}[*] Sistema suportado, instalação seguirá" ${NORM} && sleep 3s
+			{CURL} ${GIT}base.sh | bash
 			#./scripts/fedora.sh
 		else	
 			clear && echo -e "\n${VERM}[!] Sistema, DE ou versão não suportada\n" ${NORM}
